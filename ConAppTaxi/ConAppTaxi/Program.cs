33c9f65ac4977a1ad9ae8577e880d41c93e4ff1f
chanmmn <33271651+chanmmn@users.noreply.github.com>
2020-12-06 07:53:15 -0800

    Predict Taxi Fare
    Predict Taxi Fare

---
+++ ConAppTaxi/ConAppTaxi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConAppTaxiML.Model;
+
+namespace ConAppTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Add input data
+            var input = new ModelInput();
+            input.Passenger_count = 1;
+            input.Trip_time_in_secs = 1271;
+            input.Trip_distance = 3.8f;
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine(result.Score);
+        }
+    }
+}
