0e09f020b2305b4f20f8f09af0445ddaf3201559
chanmmn <33271651+chanmmn@users.noreply.github.com>
2020-12-11 07:14:34 -0800

    Commit the Diamond Price Prediction
    Commit the Diamond Price Prediction

---
+++ ConAppDiamond/ConAppDiamond/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConAppDiamondML.Model;
+using System;
+
+namespace ConAppDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Add input data
+            var input = new ModelInput();
+            input.Carot = 1.01f;
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine(result.Score);
+        }
+    }
+}
