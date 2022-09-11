699c2912a6a72867d43ab4b4a3178eaa451c8c77
chanmmn <33271651+chanmmn@users.noreply.github.com>
2022-09-11 16:37:51 +0800

    Commit ConAppMLyelp_labelledRec
    Commit ConAppMLyelp_labelledRec

---
+++ ConAppMLyelp_labelledRec/ConAppMLyelp_labelledRec/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConAppMLyelp_labelledRec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Load sample data
+            var sampleData = new MLModel1.ModelInput()
+            {
+                Column1 = @"Wow... Loved this place.",
+            };
+
+            //Load model and predict output
+            var result = MLModel1.Predict(sampleData);
+            Console.WriteLine(result.Prediction);
+        }
+    }
+}
