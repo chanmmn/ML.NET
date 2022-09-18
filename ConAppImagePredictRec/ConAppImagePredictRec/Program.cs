100aa2b99ecb98a44055842cf8ae686d67ae9642
chanmmn <33271651+chanmmn@users.noreply.github.com>
2022-09-18 23:28:39 +0800

    Commit ConAppImagePredictRec
    Commit ConAppImagePredictRec

---
+++ ConAppImagePredictRec/ConAppImagePredictRec/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConAppImagePredictRec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Load sample data
+            var imageBytes = File.ReadAllBytes(@"C:\test\ML\data\flower.jpg");
+            MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
+            {
+                ImageSource = imageBytes,
+            };
+
+            //Load model and predict output
+            var result = MLModel1.Predict(sampleData);
+            Console.WriteLine(result.PredictedLabel);
+        }
+    }
+}
